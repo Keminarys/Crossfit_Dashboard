b2b45a2b4bad4b4265c61af9ad090d5267b47bce
Keminarys <115616113+Keminarys@users.noreply.github.com>
2023-07-13 17:03:39 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -22,10 +22,15 @@ st.write('Application permettant de tracer les différents WOD de référence et
 st.divider()
 with st.sidebar.expander("Ajouter une ligne de benchmark."):
   name_ = st.multiselect('Choisir votre nom dans la liste déroulante ou Nouveau profil pour débuter.', list_Name)
+  if name_ == "Nouveau profil" : 
+    new_ = st.text_input("Merci de renseigner votre prénom uniquement.")
   type_ = st.multiselect('Choisir dans la liste déroulante le type de WOD', list_Type)
-  if type_ == 'EMOM' : ex_ = st.multiselect('Choisir dans la liste déroulante l\'exercice', list_Exercice[0])
-  if type_ == 'AMRAP' : ex_ = st.multiselect('Choisir dans la liste déroulante l\'exercice', list_Exercice[1])
-  if type_ == 'RM1' : ex_ = st.multiselect('Choisir dans la liste déroulante l\'exercice', list_Exercice[2])
+  if type_ == 'EMOM' : 
+    ex_ = st.multiselect('Choisir dans la liste déroulante l\'exercice', list_Exercice[0])
+  elif type_ == 'AMRAP' : 
+    ex_ = st.multiselect('Choisir dans la liste déroulante l\'exercice', list_Exercice[1])
+  elif type_ == 'RM1' : 
+    ex_ = st.multiselect('Choisir dans la liste déroulante l\'exercice', list_Exercice[2])
   value_ = st.number_input('Merci de renseigner la valeur.')
   unite_ = st.radio('Merci de sélectionner une unité.', list_Unité)
   date_ = st.date_input('Merci de sélectionner la date du WOD', datetime.date.today())
