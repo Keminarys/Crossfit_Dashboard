0289675727f0cd18bf015cfe3a8a99da8559cfc8
Keminarys <115616113+Keminarys@users.noreply.github.com>
2023-07-14 23:49:53 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -62,7 +62,6 @@ st.title('Crossfit83 Le Beausset')
 st.write('### Application permettant de tracer les performances dans les différents WOD de référence et ainsi voir l\'évolution de chaque athlète.')
 profile_ = st.selectbox('Merci de selectionner votre nom dans la liste déroulante', list_Name)
 st.write('Si vous ne vous trouvez pas, Merci d\'ajouter un benchmark pour continuer !')
-st.write(df.columns)
 st.divider()
 
 
@@ -102,9 +101,7 @@ with st.container() :
     if choice_2 :
         graph_ex = st.selectbox('Choisissez un type d\'exercice.', list_Exercice)
         perso = perso_df(df, profile_, chex = graph_ex)
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(df = perso, x=perso["Date"], y=perso["Valeur"], color=perso["Difficulté"],
-                    mode='lines+markers'))
+        fig = px.line(df = perso, x="Date", y="Valeur", color="Difficulté", markers=True))
         fig.update_layout(
         title=f'Progression sur l\'exercice {graph_ex}',
         autosize=False,
