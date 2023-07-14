9bf83a05c0500799921696b0bba83053e25dd49e
Keminarys <115616113+Keminarys@users.noreply.github.com>
2023-07-14 14:04:32 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -4,13 +4,30 @@ import datetime
 import streamlit as st
 import plotly.express as px
 import plotly.graph_objects as go
+from google.oauth2 import service_account
+from shillelagh.backends.apsw.db import connect
 
 ### Fonctions
 
-def load_data(sheets_url):
-    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
-    return pd.read_csv(csv_url)
-
+credentials = service_account.Credentials.from_service_account_info(
+st.secrets["gcp_service_account"], 
+scopes=["https://www.googleapis.com/auth/spreadsheets",])
+connection = connect(":memory:", adapter_kwargs={
+    "gsheetsapi" : { 
+    "service_account_info" : {
+        "type" : st.secrets["gcp_service_account"]["type"],
+        "project_id" : st.secrets["gcp_service_account"]["project_id"],
+        "private_key_id" : st.secrets["gcp_service_account"]["private_key_id"],
+        "private_key" : st.secrets["gcp_service_account"]["private_key"],
+        "client_email" : st.secrets["gcp_service_account"]["client_email"],
+        "client_id" : st.secrets["gcp_service_account"]["client_id"],
+        "auth_uri" : st.secrets["gcp_service_account"]["auth_uri"],
+        "token_uri" : st.secrets["gcp_service_account"]["token_uri"],
+        "auth_provider_x509_cert_url" : st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url" : st.secrets["gcp_service_account"]["client_x509_cert_url"],
+        }
+    },
+})
 # def form_callback(name_, type_, ex_, date_, value_, unite_, dif_):    
 #     with open('temp.csv', 'a+') as f:    #Append & read mode
 #         f.write(f"{name_}, {type_}, {ex_}, {date_}, {value_}, {unite_}, {dif_}\n")
