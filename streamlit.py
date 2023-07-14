c1a6d7ac89ca982304baf9f2a9f33b9790a39836
Keminarys <115616113+Keminarys@users.noreply.github.com>
2023-07-14 20:45:41 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -19,7 +19,7 @@ def send_to_database(row):
     sh = gc.open("Database_CF83")
     worksheet = sh.worksheet("Sheet1")
     df = pd.DataFrame(worksheet.get_all_records())
-    to_add = pd.DataFrame(row)
+    to_add = pd.DataFrame.from_dict(row)
     data = df.append(to_add,ignore_index=True)
     col = ['Nom','Type','Exercice','Date','Valeur','Unité','Difficulté']
     gspread_pandas.spread.df_to_sheet(data[col],sheet = "Sheet1",index = False)
