b00a1cad926ffa0c7bacdb943dc2d336e99308b6
Keminarys <115616113+Keminarys@users.noreply.github.com>
2023-07-17 21:56:56 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -75,6 +75,7 @@ st.title('Crossfit83 Le Beausset')
 st.write('### Application permettant de tracer les performances dans les différents WOD de référence et ainsi voir l\'évolution de chaque athlète.')
 profile_ = st.selectbox('Merci de selectionner votre nom dans la liste déroulante', list_Name)
 st.write('Si vous ne vous trouvez pas, Merci d\'ajouter votre premier WOD ou RM pour continuer !')
+st.write(list_WOD)
 st.divider()
 
 with st.sidebar :
